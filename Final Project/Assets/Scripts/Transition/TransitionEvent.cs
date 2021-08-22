117fdbc8b0e5ed129ebd07aa572eca41064536ae
Jacky Chen <jc4283@drexel.edu>
2021-08-22 16:47:01 -0400

    added a intro scene + audio + volume setting

---
+++ Final Project/Assets/Scripts/Transition/TransitionEvent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionEvent : MonoBehaviour
+{
+    [SerializeField] GameObject optionPanel;
+    public void startGame()
+    {
+        SceneManager.LoadScene("Game 1");
+    }
+
+    public void openOptions()
+    {
+        optionPanel.SetActive(true);
+    }
+
+    public void closeOptions()
+    {
+        optionPanel.SetActive(false);
+    }
+
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene("Begin");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
